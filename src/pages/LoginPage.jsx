061e9f470950f54f52a09d8bba169346bcd69ea6
felipe-lacourhughes <felipelacourhughes@gmail.com>
2024-12-15 02:30:20 -0300

    login with email

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,34 +2,83 @@ import React, { useContext, useEffect } from 'react';
 import { UserContext } from '../context/UserContext';
 import { useNavigate } from 'react-router-dom';
 
+
 const LoginPage = () => {
-  const { loginWithGoogle, user } = useContext(UserContext);
+  const { user, 
+    loginWithGoogle, 
+    isRegistering, 
+    error, 
+    email, 
+    setEmail, 
+    password, 
+    setPassword, 
+    confirmPassword, 
+    setConfirmPassword, 
+    handleRegister, 
+    handleLoginWithEmail, 
+    setIsRegistering } = useContext(UserContext);
   const navigate = useNavigate();
 
   useEffect(() => {
     if (user) {
-      // Navega sólo después de que el componente se haya montado/renderizado
       navigate('/');
     }
   }, [user, navigate]);
 
-  const handleLogin = async () => {
-    await loginWithGoogle();
-    // No es necesario redirigir aquí, el efecto se ocupará cuando user cambie
-  };
 
-  // Si el usuario no está logueado, mostramos el botón de login
-  if (!user) {
-    return (
-      <div style={{ margin: '50px' }}>
-        <h2>Iniciar Sesión con Google</h2>
-        <button onClick={handleLogin}>Login con Google</button>
+  return (
+    <div style={{ margin: '50px' }}>
+      <h2>{isRegistering ? 'Registro' : 'Iniciar Sesión'}</h2>
+
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+
+      <div style={{ marginBottom: '10px' }}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
       </div>
-    );
-  }
+      <div style={{ marginBottom: '10px' }}>
+        <input
+          type="password"
+          placeholder="Contraseña"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </div>
+      {isRegistering && (
+        <div style={{ marginBottom: '10px' }}>
+          <input
+            type="password"
+            placeholder="Confirmar Contraseña"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+          />
+        </div>
+      )}
 
-  // Si el usuario está logueado, podemos incluso retornar nulo porque el useEffect redirigirá
-  return null;
+      <div style={{ marginBottom: '10px' }}>
+        {isRegistering ? (
+          <button onClick={handleRegister}>Registrar</button>
+        ) : (
+          <button onClick={handleLoginWithEmail}>Iniciar Sesión</button>
+        )}
+      </div>
+
+      <div style={{ marginBottom: '10px' }}>
+        <button onClick={loginWithGoogle}>Login con Google</button>
+      </div>
+
+      <button onClick={() => setIsRegistering(!isRegistering)}>
+        {isRegistering ? '¿Ya tienes cuenta? Inicia sesión' : '¿No tienes cuenta? Regístrate'}
+      </button>
+    </div>
+  );
 };
 
 export default LoginPage;
